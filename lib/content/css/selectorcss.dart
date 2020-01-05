7d0039c66b6411d50ec34801c84228ead98e3bb3
crashantrax <crashantrax0@gmail.com>
2020-01-05 23:20:51 +0800

    Update CSS Selector

--- lib/content/css/selectorcss.dart
+++ lib/content/css/selectorcss.dart
@@ -1,28 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:wotproject/content/css/introcss.dart';
-import 'package:wotproject/content/css/selectorcss.dart';
 
-class Syntaxcssnav extends StatelessWidget {
+class Selectorcssnav extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "CSS Syntax",
+      title: "LEARN CSS",
       theme: new ThemeData(
         primarySwatch: Colors.black,
         primaryColor: Colors.black,
       ),
-      home: Syntaxcss(),
+      home: Selectorcss(),
     );
   }
 }
 
-class Syntaxcss extends StatelessWidget {
+class Selectorcss extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("CSS Syntax"),
+        title: new Text("CSS SELECTOR"),
       ),
       body: ListView(
         children: <Widget>[
@@ -40,7 +39,7 @@ class Syntaxcss extends StatelessWidget {
                   children: <Widget>[
                     new Padding(
                       child: new Text(
-                        "CSS Syntax",
+                        "CSS Selectors",
                         style: new TextStyle(
                             fontSize: 28.0,
                             color: Colors.black,
@@ -53,89 +52,61 @@ class Syntaxcss extends StatelessWidget {
                   ],
                 ),
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(35.0, 0.0, 10.0, 35.0),
+                  padding: const EdgeInsets.fromLTRB(35.0, 0.0, 0.0, 10.0),
                   child: new Align(
-                    alignment: Alignment.center,
+                    alignment: Alignment.centerLeft,
                     child: new Text(
-                      "A CSS rule-set consists of a selector and a declaration block:",
+                      "CSS selectors are used to 'find' (or select) the HTML elements you want to style.\n\n" +
+                          "We can divide CSS selectors into five categories:\n\n" +
+                          "- WSimple selectors (select elements based on name, id, class)\n\n"+
+                          "- Combinator selectors (select elements based on a specific relationship between them)\n\n"+
+                          "- Pseudo-class selectors (select elements based on a certain state)\n\n"+
+                          "- Pseudo-elements selectors (select and style a part of an element)\n\n"+
+                          "- Attribute selectors (select elements based on an attribute or attribute value)\n\n"
+                          "This page will explain the most basic CSS selectors.\n",
                       style: new TextStyle(
-                          fontSize: 18.0,
+                          fontSize: 13.0,
                           color: Colors.black,
-                          fontWeight: FontWeight.w500,
+                          fontWeight: FontWeight.w400,
                           fontFamily: "Ewert"),
                     ),
                   ),
                 ),
-                new Container(
-                  width: 280.0,
-                  height: 70.0,
-                  decoration: new BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    image: new DecorationImage(
-                        fit: BoxFit.fill,
-                        image: new AssetImage("images/selector.gif")),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(35.0, 35.0, 10.0, 10.0),
-                  child: new Align(
-                    alignment: Alignment.center,
-                    child: new Text(
-                      "The selector points to the HTML element you want to style.\n\n" +
-                          "The declaration block contains one or more declarations separated by semicolons.\n\n" +
-                          "Each declaration includes a CSS property name and a value, separated by a colon.\n\n" +
-                          "A CSS declaration always ends with a semicolon, and declaration blocks are surrounded by curly braces.\n",
-                      style: new TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w500,
-                          fontFamily: "Ewert"),
-                    ),
-                  ),
-                ),
-                new Card(
-                  elevation: 8.0,
-                  color: Colors.blueGrey,
-                  child: new Padding(
-                    padding: const EdgeInsets.all(18.0),
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          "In this example all <p> elements will be center-aligned, with a red text color:",
-                          style: new TextStyle(
-                              fontSize: 15.0,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
-                              fontFamily: "Ewert"),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 10.0),
-                  child: Container(
-                    color: Colors.black,
-                    child: new Align(
-                      alignment: Alignment.centerLeft,
-                      child: Padding(
-                        padding:
-                            const EdgeInsets.fromLTRB(5.0, 10.0, 0.0, 10.0),
-                        child: new Text(
-                          "\np {\n" +
-                              "   color: red;\n" +
-                              "   text-align: center\n"+
-                              "}\n",
-                          style: new TextStyle(
-                              fontSize: 18.0,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w400,
-                              fontFamily: "Ewert"),
-                        ),
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new Padding(
+                      child: new Text(
+                        "The CSS element Selector",
+                        style: new TextStyle(
+                            fontSize: 28.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w700,
+                            fontFamily: "Ewert"),
                       ),
+                      padding:
+                          const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(35.0, 0.0, 0.0, 10.0),
+                  child: new Align(
+                    alignment: Alignment.centerLeft,
+                    child: new Text(
+                      "The element selector selects HTML elements based on the element name.\n\n",
+                      style: new TextStyle(
+                          fontSize: 13.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Ewert"),
                     ),
                   ),
                 ),
+
+                /*** BUTTON ***/
                 new Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     mainAxisSize: MainAxisSize.max,
@@ -176,7 +147,7 @@ class Syntaxcss extends StatelessWidget {
                             ),
                           )),
                     ]),
-                new Row(
+                    new Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.center,
@@ -203,11 +174,9 @@ class Syntaxcss extends StatelessWidget {
     );
   }
 }
-
 Future back(context) async {
   Navigator.push(context, MaterialPageRoute(builder: (context) => Introcss()));
 }
-
 Future next(context) async {
-  Navigator.push(context, MaterialPageRoute(builder: (context) => Selectorcss()));
+  Navigator.push(context, MaterialPageRoute(builder: (context) => null));
 }
