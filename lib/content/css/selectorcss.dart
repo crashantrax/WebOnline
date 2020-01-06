1f1485fc040351cc2410a46688f3f12dfc517451
crashantrax <crashantrax0@gmail.com>
2020-01-06 12:22:49 +0800

    CSS Sel Added

--- lib/content/css/selectorcss.dart
+++ lib/content/css/selectorcss.dart
@@ -105,6 +105,392 @@ class Selectorcss extends StatelessWidget {
                     ),
                   ),
                 ),
+                new Card(
+                  elevation: 8.0,
+                  color: Colors.blueGrey,
+                  child: new Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "Example Here, all <p> elements on the page will be center-aligned, with a red text color:",
+                          style: new TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: "Ewert"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 10.0),
+                  child: Container(
+                    color: Colors.black,
+                    child: new Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(5.0, 10.0, 0.0, 10.0),
+                        child: new Text(
+                          "\np {\n" +
+                              "   color: red;\n" +
+                              "   text-align: center\n"+
+                              "}\n",
+                          style: new TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                              fontFamily: "Ewert"),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new Padding(
+                      child: new Text(
+                        "The CSS id Selector",
+                        style: new TextStyle(
+                            fontSize: 28.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w700,
+                            fontFamily: "Ewert"),
+                      ),
+                      padding:
+                          const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(35.0, 35.0, 10.0, 10.0),
+                  child: new Align(
+                    alignment: Alignment.center,
+                    child: new Text(
+                      "The id selector uses the id attribute of an HTML element to select a specific element.\n\n" +
+                          "The id of an element is unique within a page, so the id selector is used to select one unique element!\n\n"+
+                          "To select an element with a specific id, write a hash (#) character, followed by the id of the element.",
+                      style: new TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Ewert"),
+                    ),
+                  ),
+                ),
+                new Card(
+                  elevation: 8.0,
+                  color: Colors.blueGrey,
+                  child: new Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "Example The CSS rule below will be applied to the HTML element with id='para1': ",
+                          style: new TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: "Ewert"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 10.0),
+                  child: Container(
+                    color: Colors.black,
+                    child: new Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(5.0, 10.0, 0.0, 10.0),
+                        child: new Text(
+                          "\n#para1 {\n" +
+                              "   color: red;\n" +
+                              "   text-align: center\n"+
+                              "}\n",
+                          style: new TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                              fontFamily: "Ewert"),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new Padding(
+                      child: new Text(
+                        "The CSS class Selector",
+                        style: new TextStyle(
+                            fontSize: 28.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w700,
+                            fontFamily: "Ewert"),
+                      ),
+                      padding:
+                          const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(35.0, 35.0, 10.0, 10.0),
+                  child: new Align(
+                    alignment: Alignment.center,
+                    child: new Text(
+                      "The class selector selects HTML elements with a specific class attribute.\n\n" +
+                          "To select elements with a specific class, write a period (.) character, followed by the class name.\n\n",
+                      style: new TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Ewert"),
+                    ),
+                  ),
+                ),
+                new Card(
+                  elevation: 8.0,
+                  color: Colors.blueGrey,
+                  child: new Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "Example In this example all HTML elements with class='center' will be red and center-aligned: ",
+                          style: new TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: "Ewert"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 10.0),
+                  child: Container(
+                    color: Colors.black,
+                    child: new Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(5.0, 10.0, 0.0, 10.0),
+                        child: new Text(
+                          "\n.center {\n" +
+                              "   color: red;\n" +
+                              "   text-align: center\n"+
+                              "}\n",
+                          style: new TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                              fontFamily: "Ewert"),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(35.0, 35.0, 10.0, 10.0),
+                  child: new Align(
+                    alignment: Alignment.center,
+                    child: new Text(
+                      "You can also specify that only specific HTML elements should be affected by a class.\n\n",
+                      style: new TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Ewert"),
+                    ),
+                  ),
+                ),
+                new Card(
+                  elevation: 8.0,
+                  color: Colors.blueGrey,
+                  child: new Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "Example In this example only <p> elements with class='center' will be center-aligned: ",
+                          style: new TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: "Ewert"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 10.0),
+                  child: Container(
+                    color: Colors.black,
+                    child: new Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(5.0, 10.0, 0.0, 10.0),
+                        child: new Text(
+                          "\np.center {\n" +
+                              "   color: red;\n" +
+                              "   text-align: center\n"+
+                              "}\n",
+                          style: new TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                              fontFamily: "Ewert"),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(35.0, 35.0, 10.0, 10.0),
+                  child: new Align(
+                    alignment: Alignment.center,
+                    child: new Text(
+                      "HTML elements can also refer to more than one class.\n\n",
+                      style: new TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Ewert"),
+                    ),
+                  ),
+                ),
+                new Card(
+                  elevation: 8.0,
+                  color: Colors.blueGrey,
+                  child: new Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "Example In this example the <p> element will be styled according to class='center' and to class='large': ",
+                          style: new TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: "Ewert"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 10.0),
+                  child: Container(
+                    color: Colors.black,
+                    child: new Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(5.0, 10.0, 0.0, 10.0),
+                        child: new Text(
+                          "\n<p class='center large'>This paragraph refers to two classes.</p>\n",
+                          style: new TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                              fontFamily: "Ewert"),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new Padding(
+                      child: new Text(
+                        "The CSS Universal Selector",
+                        style: new TextStyle(
+                            fontSize: 28.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w700,
+                            fontFamily: "Ewert"),
+                      ),
+                      padding:
+                          const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(35.0, 35.0, 10.0, 10.0),
+                  child: new Align(
+                    alignment: Alignment.center,
+                    child: new Text(
+                      "The universal selector (*) selects all HTML elements on the page.\n\n",
+                      style: new TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Ewert"),
+                    ),
+                  ),
+                ),
+                new Card(
+                  elevation: 8.0,
+                  color: Colors.blueGrey,
+                  child: new Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "Example The CSS rule below will affect every HTML element on the page: ",
+                          style: new TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: "Ewert"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 10.0),
+                  child: Container(
+                    color: Colors.black,
+                    child: new Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(5.0, 10.0, 0.0, 10.0),
+                        child: new Text(
+                          "\n* {\n" +
+                              "   color: red;\n" +
+                              "   text-align: center\n"+
+                              "}\n",
+                          style: new TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                              fontFamily: "Ewert"),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
 
                 /*** BUTTON ***/
                 new Row(
